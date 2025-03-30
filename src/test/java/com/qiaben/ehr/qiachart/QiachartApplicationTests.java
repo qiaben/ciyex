d58e0fdba321d2170e53c256b3822929fb606c15
Sivakumar Dhanapal <siva@qiaben.com>
2025-03-30 16:01:52 -0600

    Initial commit

---
+++ src/test/java/com/qiaben/ehr/qiachart/QiachartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiaben.ehr.qiachart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiachartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
