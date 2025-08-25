374a6943aaba3a578ae389f532fed56415f20c30
JyothiraravindhKumar <jyothir@hinisoft.com>
2025-08-25 05:37:27 +0530

    Added the Service, Entity, Controller, DTO, Respository, FhirExternalStorage for the ReferralProvider

---
+++ src/main/java/com/qiaben/ciyex/dto/ReferralProviderDto.java
@@ -0,0 +1,32 @@
+package com.qiaben.ciyex.dto;
+
+import lombok.Data;
+
+@Data
+public class ReferralProviderDto {
+    private Long id;
+    private String name;
+    private String specialty;
+    private String address;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+    private String phoneNumber;
+    private String email;
+    private String fhirId;
+    private PracticeInfo practice;
+
+    @Data
+    public static class PracticeInfo {
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    public static class Audit {
+        private String createdDate;
+        private String lastModifiedDate;
+    }
+    private Audit audit = new Audit();
+}
