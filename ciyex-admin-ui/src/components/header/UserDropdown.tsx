2ef3a36e42f186ae9b12f34877b6d7bf37e8563e
Mahendran <mahendran@hinisoft.com>
2025-09-20 12:59:36 +0530

    feat: implement user address management with editable fields and local storage integration

--- ciyex-admin-ui/src/components/header/UserDropdown.tsx
+++ ciyex-admin-ui/src/components/header/UserDropdown.tsx
@@ -159,7 +159,7 @@ export default function UserDropdown() {
             <li>
                 <DropdownItem
                     onItemClick={closeDropdown} // Handle the click event to toggle the context switch dropdown
-                    href="/practice-switch"
+                    href="/super-admin-practice-switch"
                     tag="a"
                     className="flex items-center gap-3 px-3 py-2 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
                 >
