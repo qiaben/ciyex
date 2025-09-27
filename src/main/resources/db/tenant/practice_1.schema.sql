dbdd5c89b333f2eb3924351cfcdeb429eb3b42ab
Sivakumar Dhanapal <shiva.dhanapal@gmail.com>
2025-09-26 18:23:06 -0600

    fix(migration): update org_config insertion logic to handle missing unique constraint

--- src/main/resources/db/tenant/practice_1.schema.sql
+++ src/main/resources/db/tenant/practice_1.schema.sql
@@ -13,7 +13,7 @@ SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
 SET xmloption = content;
-SET client_min_messages = warning;cccccbuljlrnnvnfkblkdkeleirnvvijnejjkfuindnf
+SET client_min_messages = warning;
 
 SET row_security = off;
 
