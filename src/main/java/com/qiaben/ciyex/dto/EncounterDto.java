391f27b6d034787b785544098d3ad89b4b42e406
Sivakumar Dhanapal <shiva.dhanapal@gmail.com>
2025-10-27 17:15:48 -0600

    refactor: remove commented code from EncounterDto
    - Deleted redundant commented duplicate of EncounterDto class definition
    - Cleaned up file formatting by removing trailing empty lines

--- src/main/java/com/qiaben/ciyex/dto/EncounterDto.java
+++ src/main/java/com/qiaben/ciyex/dto/EncounterDto.java
@@ -30,24 +30,3 @@ public class EncounterDto {
 
 
 }
-
-//package com.qiaben.ciyex.dto;
-//
-//import com.qiaben.ciyex.entity.EncounterStatus;
-//import lombok.*;
-//import java.time.Instant;
-//
-//@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
-//public class EncounterDto {
-//    private Long id;
-//    private Long orgId;
-//    private Long patientId;
-//    private String visitCategory;
-//    private String encounterProvider;
-//    private String type;
-//    private String sensitivity;
-//    private String dischargeDisposition;
-//    private String reasonForVisit;
-//    private Instant encounterDate;
-//    private EncounterStatus status;
-//}
